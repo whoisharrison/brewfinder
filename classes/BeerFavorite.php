36cd9f4c19eb8e0b29d7af36e0bde2bc2770bfb6
Luc Flynn <lucnmsu@gmail.com>
2017-05-27 14:23:32 -0600

    added author doc bloc to BeerFav class

---
+++ classes/BeerFavorite.php
@@ -0,0 +1,13 @@
+<?php
+require_once("autoload.php");
+
+/*
+ * brewfinder BeerFavorite class
+ *
+ * @author Luc FLynn <lucnmsu@gmail.com>
+ * @coauthors & @editors: Billy Trabaudo, Lea McDuffie, Michael Harrison, Shihlin Lu
+ * version 0.0.0
+ * */
+
+
+
