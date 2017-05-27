02423ca5351b41673e74fe770e096f78bab244f1
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-27 16:19:47 -0600

    Variables, listed.

--- php/classes/Brewery.php
+++ php/classes/Brewery.php
@@ -15,6 +15,62 @@ class Brewery implements \JsonSerializable {
 	private $breweryId;
 	/**
 	 * Token to verify the brewery is valid.
-	 * @var string $brewery
+	 * @var int $breweryProfileId
+	 **/
+	private $breweryProfileId;
+	/**
+	 * Token to verify the brewery profile is valid.
+	 * @var string $breweryActivationToken
+	 **/
+	private $breweryActivationToken;
+	/**
+	 * Brewery location x
+	 * @var float $breweryAddress1
+	 **/
+	private $breweryAddress1;
+	/**
+	 * Brewery location y
+	 * @var float $breweryAddress2
+	 **/
+	private $breweryAddress2;
+	/**
+	 * @var string $breweryCity
 	 */
+	private $breweryCity;
+	/**
+	 * @var string $breweryContent
+	 */
+	private $breweryContent;
+	/**
+	 * @var string $breweryEmail
+	 */
+	private $breweryEmail;
+	/**
+	 * @var string $breweryHash
+	 */
+	private $breweryHash;
+	/**
+	 * @var int|null $breweryImageId
+	 */
+	private $breweryImageId;
+	/**
+	 * @var string $breweryName
+	 */
+	private $breweryName;
+	/**
+	 * @var string $breweryPhone
+	 */
+	private $breweryPhone;
+	/**
+	 * @var $brewerySalt
+	 */
+	private $brewerySalt;
+	/**
+	 * @var
+	 */
+	private $breweryState;
+	/**
+	 * @var int $breweryZip
+	 */
+	private $breweryZip;
 }
