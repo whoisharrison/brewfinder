5ce63c3b5b3ac841a7a5db644d636f2a35106db5
Shihlin Lu <shihlinlu@gmail.com>
2017-05-27 14:26:06 -0600

    created tables

--- sql/brewfinder.sql
+++ sql/brewfinder.sql
@@ -2,5 +2,34 @@ DROP TABLE IF EXISTS beerfavorite;
 DROP TABLE IF EXISTS beerimage ;
 DROP TABLE IF EXISTS breweryimage;
 DROP TABLE IF EXISTS brewery;
+DROP TABLE IF EXISTS image;
 DROP TABLE IF EXISTS beer;
 DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile (
+
+);
+
+CREATE TABLE beer (
+
+);
+
+CREATE TABLE image (
+
+);
+
+CREATE TABLE brewery (
+
+);
+
+CREATE TABLE breweryImage (
+
+);
+
+CREATE TABLE beerImage (
+
+);
+
+CREATE TABLE beerFavorite (
+
+);
