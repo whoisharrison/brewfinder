871150bbfc57b4c74bc4de2cb1eb30d5f46fc217
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-27 18:11:39 -0600

    Accessor and mutator for brewery address 2, location x, and location y, done.

--- php/classes/Brewery.php
+++ php/classes/Brewery.php
@@ -24,12 +24,12 @@ class Brewery implements \JsonSerializable {
 	 **/
 	private $breweryActivationToken;
 	/**
-	 * Brewery location x
+	 * Brewery address 1
 	 * @var float $breweryAddress1
 	 **/
 	private $breweryAddress1;
 	/**
-	 * Brewery location y
+	 * Brewery address 2
 	 * @var float $breweryAddress2
 	 **/
 	private $breweryAddress2;
@@ -53,6 +53,16 @@ class Brewery implements \JsonSerializable {
 	 * @var int|null $breweryImageId
 	 */
 	private $breweryImageId;
+	/**
+	 * location x of this Brewery.
+	 * @var float $breweryLocationX
+	 **/
+	private $breweryLocationX;
+	/**
+	 * location y of this Brewery.
+	 * @var float $breweryLocationY
+	 **/
+	private $breweryLocationY;
 	/**
 	 * @var string $breweryName
 	 */
@@ -172,7 +182,7 @@ class Brewery implements \JsonSerializable {
 		}
 
 		// Verify the brewery profile id is positive.
-		if($newBreweryProfileId <=0) {
+		if($newBreweryProfileId <= 0) {
 			throw(new \RangeException("brewery profile id is not positive"));
 		}
 
@@ -184,9 +194,11 @@ class Brewery implements \JsonSerializable {
 		 *
 		 * @return string value for address 1
 		 **/
-		public function getBreweryAddress1(): string {
-		return ($this->breweryAddress1);
+		public
+		function getBreweryAddress1(): string {
+			return ($this->breweryAddress1);
 		}
+	}
 
 		/**
 		 * Mutator method for address 1
@@ -196,7 +208,8 @@ class Brewery implements \JsonSerializable {
 		 * @throws \RangeException if $newBreweryAddress1 is > 128 characters
 		 * @throws \TypeError if the $newBreweryAddress1 is not a string
 		 **/
-		public function setBreweryAddress1(string $newBreweryAddress1) : void {
+		public
+		function setBreweryAddress1(string $newBreweryAddress1): void {
 
 			// Verify address 1 is secure
 			$newBreweryAddress1 = trim($newBreweryAddress1);
@@ -214,5 +227,109 @@ class Brewery implements \JsonSerializable {
 			$this->breweryAddress1 = $newBreweryAddress1;
 		}
 
-	}
+		/**
+		 * Accessor method for address 2
+		 *
+		 * @return string|null value for address 2
+		 **/
+		public
+		function getBreweryAddress2(): ?string {
+			return ($this->breweryAddress2);
+		}
+
+		/**
+		 * Mutator method for address 2
+		 *
+		 * @param string|null $newBreweryAddress2
+		 * @throws \InvalidArgumentException if $newBreweryAddress2 is not a string or insecure
+		 * @throws \RangeException if $newBreweryAddress2 is > 128 characters
+		 * @throws \TypeError if the $newBreweryAddress2 is not a string
+		 **/
+		public
+		function setBreweryAddress2(?string $newBreweryAddress2): void {
+
+			// Verify address 2 is secure
+			$newBreweryAddress2 = trim($newBreweryAddress2);
+			$newBreweryAddress2 = filter_var($newBreweryAddress2, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+			if(empty($newBreweryAddress2) === true) {
+				throw(new \InvalidArgumentException("brewery address 2 is empty or insecure"));
+			}
+
+			// Verify address 2 will fit in the database.
+			if(strlen($newBreweryAddress2) > 128) {
+				throw(new \RangeException("brewery address 2 is too large"));
+			}
+
+			// Store address 2.
+			$this->breweryAddress2 = $newBreweryAddress2;
+		}
+
+		/**
+		 * Accessor method for brewery location x.
+		 *
+		 * @param float $breweryLocationX
+		 * @return float $breweryLocationX value of brewery location x
+		 **/
+		public
+		function getBreweryLocationX() {
+			return ($this->breweryLocationX);
+		}
+
+		/**
+		 * Mutator method for brewery location x.
+		 *
+		 * @param float @newBreweryLocationX new value of brewery location x
+		 * @throws \RangeException if $newBreweryLocationX is not between -180 and 180
+		 * @throws \TypeError if $newBreweryLocationX is not an integer
+		 **/
+		public function setBreweryLocationX(float $newBreweryLocationX) {
+			$newBreweryLocationX = $newBreweryLocationX == 0.0 ? 0.0 : filter_var($newBreweryLocationX, FILTER_VALIDATE_FLOAT);
+
+			// If the brewery location x is null, immediately return it.
+			if($newBreweryLocationX === false) {
+				throw(new \InvalidArgumentException("location x is not a valid data type"));
+			}
+
+			// Verify that brewery location x is positive.
+			if($newBreweryLocationX < -180 || $newBreweryLocationX > 180) {
+				throw(new \RangeException("location x is not within range"));
+			}
+
+			// Convert and store brewery location x.
+			$this->breweryLocationX = $newBreweryLocationX;
+		}
+
+		/**
+		 * Accessor method for brewery location y.
+		 *
+		 * @param float $breweryLocationY
+		 * @return float $breweryLocationY value of brewery location y
+		 **/
+		public function getBreweryLocationY() {
+			return($this->breweryLocationY);
+		}
+
+		/**
+		 * Mutator method for brewery location y.
+		 *
+		 * @param float @newBreweryLocationY new value of profile location y
+		 * @throws \RangeException if $newBreweryLocationY is not between -90 and 90
+		 * @throws \TypeError if $newBreweryLocationY is not an integer
+		 **/
+		public function setBreweryLocationY(float $newBreweryLocationY) {
+			$newBreweryLocationY = $newBreweryLocationY == 0.0 ? 0.0 : filter_var($newBreweryLocationY, FILTER_VALIDATE_FLOAT);
+
+			// If the brewery location y is null, immediately return it.
+			if($newBreweryLocationY === false) {
+				throw(new \InvalidArgumentException("location y is not a valid data type"));
+			}
+
+			// Verify that brewery location y is positive.
+			if($newBreweryLocationY < -90 || $newBreweryLocationY > 90) {
+				throw(new \RangeException("location y is not within range"));
+			}
+
+			// Convert and store brewery location y.
+			$this->breweryLocationY = $newBreweryLocationY;
+		}
 }
