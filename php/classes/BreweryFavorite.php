abe749531f8e788f8a6ecf1acc17ed8c051cfed3
Luc Flynn <lucnmsu@gmail.com>
2017-05-27 21:08:28 -0600

    brewery favorite class rought draft complete!

--- php/classes/BreweryFavorite.php
+++ php/classes/BreweryFavorite.php
@@ -254,50 +254,50 @@ class BreweryFavorite implements \JsonSerializable {
 	 * gets the favorite by beer id etc
 	 *
 	 * @param \PDO $pdo PDO connection obect
-	 * @param int $beerFavoriteId to search for
-	 * @param int $beerFavoriteBeerId to search for
-	 * @param int $beerFavoriteProfileId to search for
+	 * @param int $breweryFavoriteId to search for
+	 * @param int $breweryFavoriteBreweryId to search for
+	 * @param int $breweryFavoriteProfileId to search for
 	 * @param \DateTime $beerFavoriteDateTim
 	 *
 	 * return beerFavorite
 	 */
-	public static function getByAll(\PDO $pdo, int $beerFavoriteId, int $beerFavoriteBeerId, int $beerFavoriteProfileId, \DateTime $beerFavoriteDateTime) : ?BreweryFavorite {
+	public static function getByAll(\PDO $pdo, int $breweryFavoriteId, int $breweryFavoriteBreweryId, int $breweryFavoriteProfileId, \DateTime $breweryFavoriteDateTime) : ?BreweryFavorite {
 		// sanitize the faovite id before search
-		if($beerFavoriteId <= 0) {
+		if($breweryFavoriteId <= 0) {
 			throw(new \PDOException("Id not positive"));
 		}
-		if($beerFavoriteBeerId <= 0) {
+		if($breweryFavoriteBreweryId <= 0) {
 			throw(new \PDOException("Id not positive"));
 		}
-		if($beerFavoriteProfileId <= 0) {
+		if($breweryFavoriteProfileId <= 0) {
 			throw(new \PDOException("Id not positive"));
 		}
-		if($beerFavoriteDateTime <= 0) {
+		if($breweryFavoriteDateTime <= 0) {
 			throw(new\PDOException("Can't have negative Time"));
 		}
 		
 		
 		// create query table
-		$query = "SELECT beerFavoriteId, beerFavoriteBeerId, beerFavoriteProfileId, beerFavoriteDateTime FROM BeerFavorite WHERE beerFavoriteId =:beerFavoriteId AND beerFavoriteBeerId = :beerFavoriteBeerId AND beerFavoriteProfileId = :beerFavoriteProfileId AND beerFavoriteDateTime = :beerFavoriteDateTime";
+		$query = "SELECT breweryFavoriteId, breweryFavoriteBeerId, breweryFavoriteProfileId, breweryFavoriteDateTime FROM BreweryFavorite WHERE breweryFavoriteId = :breweryFavoriteId AND breweryFavoriteBeerId = :breweryFavoriteBreweryId AND breweryFavoriteProfileId = :breweryFavoriteProfileId AND breweryFavoriteDateTime = :breweryFavoriteDateTime";
 		$statement = $pdo->prepare($query);
 		//bind the placeholders
-		$parameters = ["beerFavoriteId" => $beerFavoriteId, "beerFavoriteBeerId" => $beerFavoriteBeerId, "beerFavoriteProfileId" => $beerFavoriteProfileId, "beerFavoriteDateTime" => $beerFavoriteDateTime];
+		$parameters = ["breweryFavoriteId" => $breweryFavoriteId, "breweryFavoriteBeerId" => $breweryFavoriteBreweryId, "breweryFavoriteProfileId" => $breweryFavoriteProfileId, "breweryFavoriteDateTime" => $breweryFavoriteDateTime];
 		$statement->execute($parameters);
 		
 		
 		// grab the favorite from mySQL
 		try {
-			$beerFavorite = null;
+			$breweryFavorite = null;
 			$statement->setFetchMode(\PDO::FETCH_ASSOC);
 			$row = $statement->fetch();
 			if($row !== false ) {
-				$beerFavorite = new BreweryFavorite($row ["beerFavoriteId"], $row["beerFavoriteBeerId"], $row["beerFavoriteProfileId"], $row["beerFavoriteDateTime"]);
+				$breweryFavorite = new BreweryFavorite($row ["breweryFavoriteId"], $row["breweryFavoriteBreweryId"], $row["brewertFavoriteProfileId"], $row["breweryFavoriteDateTime"]);
 			}
 		}    catch(\Exception $exception) {
 			//if the row can't convert rethrow it
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
-		return ($beerFavorite);
+		return ($breweryFavorite);
 	}
 	
 	/**
@@ -308,34 +308,34 @@ class BreweryFavorite implements \JsonSerializable {
 	 * @param int $beerFavoriteBeerId to search for
 	 * @param int $beerFavoriteProfileid to search for
 	 */
-	public static function getFavoriteByBeerFavoriteId(\PDO $pdo, int $beerFavoriteId) : \SPLFixedArray {
+	public static function getFavoriteByBreweryFavoriteId(\PDO $pdo, int $breweryFavoriteId) : \SPLFixedArray {
 		// sanitize the faovite id before search
-		if($beerFavoriteId <= 0) {
+		if($breweryFavoriteId <= 0) {
 			throw(new \PDOException("Id not positive"));
 		}
 		// create query table
-		$query = "SELECT beerFavoriteId beerFavoriteProfileId, beerFavoriteBeerId, beerFavoriteDateTime FROM BeerFavorite WHERE beerFavoriteId = :beerFavoriteId";
+		$query = "SELECT breweryFavoriteId, breweryFavoriteProfileId, breweryFavoriteBeerId, breweryFavoriteDateTime FROM BreweryFavorite WHERE breweryFavoriteId = :breweryFavoriteId";
 		$statement = $pdo->prepare($query);
 		//bind the placeholders
-		$parameters = ["beerFavoriteId" => $beerFavoriteId];
+		$parameters = ["breweryFavoriteId" => $breweryFavoriteId];
 		$statement->execute($parameters);
 		
 		//build array of favorites
-		$beerFavorites = new \SplFixedArray($statement->rowCount());
+		$breweryFavorites = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		
 		while(($row = $statement->fetch()) !== false) {
 			 try {
-				 $beerFavorite = new BreweryFavorite($row["beerFavoriteId"], $row ["beerFavoriteProfileId"], $row ["beerFavoriteBeerId"], $row["beerFavoriteDateTime"]);
-				 $beerFavorites[$beerFavorites->key()] = $beerFavorite;
-				 $beerFavorites->next();
+				 $beerFavorite = new BreweryFavorite($row["breweryFavoriteId"], $row ["breweryFavoriteProfileId"], $row ["breweryFavoriteBreweryId"], $row["breweryFavoriteDateTime"]);
+				 $breweryFavorites[$breweryFavorites->key()] = $beerFavorite;
+				 $breweryFavorites->next();
 			 } catch (\Exception $exception) {
 			 	// if the row couldn't be converted rethrow it
 				 throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		
 		}
-		return ($beerFavorites);
+		return ($breweryFavorites);
 	}
 	
 	/**
@@ -346,34 +346,34 @@ class BreweryFavorite implements \JsonSerializable {
 	 * @param int $beerFavoriteBeerId to search for
 	 * @param int $beerFavoriteProfileid to search for
 	 */
-	public static function getFavoriteByBeerProfileId(\PDO $pdo, int $beerFavoriteProfileId) : \SPLFixedArray {
+	public static function getFavoriteByBreweryProfileId(\PDO $pdo, int $breweryFavoriteProfileId) : \SPLFixedArray {
 		// sanitize the faovite id before search
-		if($beerFavoriteProfileId <= 0) {
+		if($breweryFavoriteProfileId <= 0) {
 			throw(new \PDOException("Id not positive"));
 		}
 		// create query table
-		$query = "SELECT beerFavoriteId, beerFavoriteProfileId, beerFavoriteBeerId, beerFavoriteDatetime FROM BeerFavorite WHERE beerFavoriteProfileId = :beerFavoriteProfileId";
+		$query = "SELECT breweryFavoriteId, breweryFavoriteProfileId, breweryFavoriteBeerId, breweryFavoriteDateTime FROM BreweryFavorite WHERE breweryFavoriteProfileId = :breweryFavoriteProfileId";
 		$statement = $pdo->prepare($query);
 		//bind the placeholders
-		$parameters = ["beerFavoriteId" => $beerFavoriteProfileId];
+		$parameters = ["breweryFavoriteId" => $breweryFavoriteProfileId];
 		$statement->execute($parameters);
 		
 		//build array of favorites
-		$beerFavorites = new \SplFixedArray($statement->rowCount());
+		$breweryFavorites = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		
 		while(($row = $statement->fetch()) !== false) {
 			try {
-				$beerFavorite = new BreweryFavorite($row["beerFavoriteId"], $row ["beerFavoriteProfileId"], $row ["beerFavoriteBeerId"], $row["beerFavoriteDateTime"]);
-				$beerFavorites[$beerFavorites->key()] = $beerFavorite;
-				$beerFavorites->next();
+				$breweryFavorite = new new BreweryFavorite($row["breweryFavoriteId"], $row ["breweryFavoriteProfileId"], $row ["breweryFavoriteBreweryId"], $row["breweryFavoriteDateTime"]);
+				$breweryFavorites[$breweryFavorites->key()] = $breweryFavorite;
+				$breweryFavorites->next();
 			} catch (\Exception $exception) {
 				// if the row couldn't be converted rethrow it
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 			
 		}
-		return ($beerFavorites);
+		return ($breweryFavorites);
 	}
 	
 	/**
@@ -381,59 +381,59 @@ class BreweryFavorite implements \JsonSerializable {
 	 *
 	 * @param \PDO $pdo PDO connection obect
 	 * @param int $beerFavoriteId to search for
-	 * @param int $beerFavoriteBeerId to search for
+	 * @param int $breweryFavoriteBreweryId to search for
 	 * @param int $beerFavoriteProfileid to search for
 	 */
-	public static function getFavoriteByBeerFavoriteBeerId(\PDO $pdo, int $beerFavoriteBeerId) : \SPLFixedArray {
+	public static function getFavoriteByBreweryFavoriteBreweryId(\PDO $pdo, int $breweryFavoriteBreweryId) : \SPLFixedArray {
 		// sanitize the favorite id before search
-		if($beerFavoriteBeerId <= 0) {
+		if($breweryFavoriteBreweryId <= 0) {
 			throw(new \PDOException("Id not positive"));
 		}
 		// create query table
-		$query = "SELECT beerFavoriteId beerFavoriteProfileId, beerFavoriteBeeId beerFavoriteDateTime FROM BeerFavorite WHERE beerFavoriteBeerId = :beerFavoriteBeerId";
+		$query = "SELECT breweryFavoriteId, breweryFavoriteProfileId, breweryFavoriteBeerId, breweryFavoriteDateTime FROM BreweryFavorite WHERE breweryFavoriteBreweryId = :breweryFavoriteBreweryId";
 		$statement = $pdo->prepare($query);
 		//bind the placeholders
-		$parameters = ["beerFavoriteBeerId" => $beerFavoriteBeerId];
+		$parameters = ["brewertFavoriteBreweryId" => $breweryFavoriteBreweryId];
 		$statement->execute($parameters);
 		
 		//build array of favorites
-		$beerFavorites = new \SplFixedArray($statement->rowCount());
+		$breweryFavorites = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		
 		while(($row = $statement->fetch()) !== false) {
 			try {
-				$beerFavorite = new BreweryFavorite($row["beerFavoriteId"], $row ["beerFavoriteProfileId"], $row ["beerFavoriteBeerId"], $row ["beerFavoriteDateTime"]);
-				$beerFavorites[$beerFavorites->key()] = $beerFavorite;
-				$beerFavorites->next();
+				$breweryFavorite = newBreweryFavorite($row["breweryFavoriteId"], $row ["breweryFavoriteProfileId"], $row ["breweryFavoriteBreweryId"], $row["breweryFavoriteDateTime"]);
+				$breweryFavorites[$breweryFavorites->key()] = $breweryFavorite;
+				$breweryFavorites->next();
 			} catch (\Exception $exception) {
 				// if the row couldn't be converted rethrow it
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 			
 		}
-		return ($beerFavorites);
+		return ($breweryFavorites);
 	}
  /**
 * gets an array of readings based on its date
 *
 * @param \PDO $pdo connection object
-* @param \DateTime $beerFavoriteSunriseDateTime beginning date to search for
-* @param \DateTime $beerFavoriteSunsetDateTime ending date to search for
+* @param \DateTime $breweryFavoriteSunriseDateTime beginning date to search for
+* @param \DateTime $breweryFavoriteSunsetDateTime ending date to search for
 * @return \SplFixedArray of readings found
 * @throws \PDOException when mySQL related errors occur
 * @throws \TypeError when variables are not the correct dates are in the wrong format
 * @throws \InvalidArgumentException if either sun dates are in the wrong format
 **/
  
-	public static function getFavoriteByBeerFavoriteDateTime (\PDO $pdo, $beerFavoriteSunriseDateTime, $beerFavoriteSunsetDateTime) : \SplFixedArray {
+	public static function getFavoriteByBeerFavoriteDateTime (\PDO $pdo, $breweryFavoriteSunriseDateTime, $breweryFavoriteSunsetDateTime) : \SplFixedArray {
 		//enforce both date are present
-		if((empty ($beerFavoriteSunriseDateTime) === true) || (empty ($beerFavoriteSunsetDateTime) === true)) {
+		if((empty ($breweryFavoriteSunriseDateTime) === true) || (empty ($breweryFavoriteSunsetDateTime) === true)) {
 			throw (new \InvalidArgumentException("dates are empty or insecure"));
 		}
 		//ensure both dates are in the correct format and are in the correct format and are secure
 		try{
-			$beerFavoriteSunriseDateTime = self::validateDateTime($beerFavoriteSunriseDateTime);
-			$beerFavoriteSunsetDateTime = self::validateDateTime($beerFavoriteSunsetDateTime);
+			$breweryFavoriteSunriseDateTime = self::validateDateTime($breweryFavoriteSunriseDateTime);
+			$breweryFavoriteSunsetDateTime = self::validateDateTime($breweryFavoriteSunsetDateTime);
 		} catch(\InvalidArgumentException | \RangeException $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
@@ -441,32 +441,32 @@ class BreweryFavorite implements \JsonSerializable {
 		
 		
 		//create query template
-		$query = "SELECT beerFavoriteId, beerFavoriteProfileId, beerFavoriteBeerId, beerFavoriteDateTime from BeerFavorite WHERE beerFavoriteDateTime >= :beerFavoriteSunriseDateTime AND beerFavoriteDateTime <= :beerFavoriteSunsetDateTime LIMIT :startRow, :pageSize ";
+		$query = "SELECT breweryFavoriteId, breweryFavoriteProfileId, breweryFavoriteBeerId, breweryFavoriteDateTime FROM BreweryFavorite WHERE breweryFavoriteDateTime >= :breweryFavoriteSunriseDateTime AND breweryFavoriteDateTime <= :breweryFavoriteSunsetDateTime LIMIT :startRow, :pageSize ";
 		$statement= $pdo->prepare($query);
 		
 		
 		//format the dates so that mySQL can use them
-		$formattedBeerFavoriteSunriseDateTime = $beerFavoriteSunriseDateTime->format("Y-m-d H:i:s.u");
-		$formattedBeerFavoriteSunsetDate = $beerFavoriteSunsetDateTime->format("Y-m-d H:i:s.u");
-		$parameters = ["beerFavoriteSunriseDateTime" => $formattedBeerFavoriteSunriseDateTime, "beerFavoriteSunsetDate" => $formattedBeerFavoriteSunsetDate];
+		$formattedBreweryFavoriteSunriseDateTime = $breweryFavoriteSunriseDateTime->format("Y-m-d H:i:s.u");
+		$formattedBreweryFavoriteSunsetDate = $breweryFavoriteSunsetDateTime->format("Y-m-d H:i:s.u");
+		$parameters = ["breweryFavoriteSunriseDateTime" => $formattedBreweryFavoriteSunriseDateTime, "breweryFavoriteSunsetDate" => $formattedBreweryFavoriteSunsetDate];
 		$statement->bindParam(":startRow", $startRow, \PDO::PARAM_INT);
 		$statement->bindParam(":pageSize", self::$pageSize);
 		$statement->execute($parameters);
 		
 		
 		//build an array of readings
-		$beerFavorites = new \SplFixedArray($statement->rowCount());
+		$breweryFavorites = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		while(($row = $statement->fetch()) !== false) {
 			try{
-				$beerFavorite = new BreweryFavorite($row["beerFavoriteId"], $row ["beerFavoriteProfileId"], $row ["beerFavoriteBeerId"], $row["beerFavoriteDateTime"]);
-				$beerFavorites[$beerFavorites->key()] = $beerFavorite;
-				$beerFavorites->next();
+				$beerFavorite = new BreweryFavorite($row["breweryFavoriteId"], $row ["breweryFavoriteProfileId"], $row ["breweryFavoriteBreweryId"], $row["breweryFavoriteDateTime"]);
+				$breweryFavorites[$breweryFavorites->key()] = $breweryFavorites;
+				$breweryFavorites->next();
 			} catch(\Exception $exception) {
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
-		return($beerFavorites);
+		return($breweryFavorites);
 	}
 	
 	/**
